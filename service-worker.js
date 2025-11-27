ef8cd950eff4d774d77c4c6ae039029e50b82ff3
jhonmelendez40 <jhonmelendez40@gmail.com>
2025-11-27 15:53:23 -0500

    Implement service worker for PWA caching
    Add service worker for caching assets and handling fetch requests.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,25 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('pwa-music-book-v1').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'css/style.css',
+        'js/app.js',
+        'views/login.html',
+        'views/register.html',
+        'views/recover.html',
+        'views/main.html',
+        'img/icon-192.png',
+        'img/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
 
